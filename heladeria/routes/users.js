777b325cdb677d3e02f802c824ae1ba8c957fdc9
josuedejesus <joshuadejesus777@gmail.com>
2025-08-19 17:18:22 -0600

    endpoints

--- heladeria/routes/users.js
+++ heladeria/routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/get-hola', function(req, res, next) {
+  res.send('Hola');
+})
+
+router.post('/post-hola', function(req, res, next) {
+  res.send('POST hola');
+})
+
+router.put('/put-hola', function(req, res, next) {
+  res.send('PUT hola');
+})
+
+router.delete('/delete-hola', function(req, res, next) {
+  res.send('DELETE hola');
+})
+
 module.exports = router;
